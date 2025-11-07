60d3c4dc174ae93a003466287ccca99aec58d3e2
Swap1980-as <swap1980@mail.ru>
2025-11-07 15:53:19 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
 from app import app
 
+# Конфигурация для Gunicorn
+bind = "0.0.0.0:10000"
+workers = 1
+worker_class = "sync"
+timeout = 120  # Увеличиваем таймаут до 2 минут
+
 if __name__ == "__main__":
     app.run()
